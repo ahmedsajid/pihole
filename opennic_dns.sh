d4eaac13960154f2dec1757b0188b10f16235062
Ahmed Sajid <ahmed4343@hotmail.com>
2019-01-12 13:25:38 -0500

    Updated opennic_dns.sh
    Removed unused vars and added comments

--- opennic_dns.sh
+++ opennic_dns.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-# This script queries opennic API endpoint and gets DNS Server
-# Then dumps those in dnsmasq and pihole setupvars
+#
+# This script is used as a cronjob to perform update to DNS Settings.
+# It queries opennic API endpoint and gets DNS Server list then update dnsmasq and pihole setupvars files with the retrieved DNS server IPs
+#
 
-IPv4=`curl -s "https://api.opennicproject.org/geoip/?bare&res=2@rnd=true&anon=true&ipv=4" --output /tmp/ipv4`
-IPv6=`curl -s "https://api.opennicproject.org/geoip/?bare&res=2@rnd=true&anon=true&ipv=6" --output /tmp/ipv6`
-
-# Var for index
-i=1
+# API call to get DNS servres
+# Get 2 random IPv4 DNS with anonymized logs
+curl -s "https://api.opennicproject.org/geoip/?bare&res=2@rnd=true&anon=true&ipv=4" --output /tmp/ipv4
+# Get 2 random IPv6 DNS with anonymized logs
+curl -s "https://api.opennicproject.org/geoip/?bare&res=2@rnd=true&anon=true&ipv=6" --output /tmp/ipv6
 
 # If both IPV4 and IPV6 are retrieved only then perform the update to the files
 if  [ -f "/tmp/ipv4" ] && [ -f "/tmp/ipv6" ]; then
+
+    # Combining both files into one
     cat /tmp/ipv4 /tmp/ipv6 > /tmp/ip
+
+    # Delete current DNS settings from setup and pihole.conf file
     sed -i '/^PIHOLE_DNS/d' /etc/pihole/setupVars.conf
     sed -i '/^server/d' /etc/dnsmasq.d/01-pihole.conf
 
+    # Loop through DNS IPs and echo them into setup and pihole.conf file
     while read ip
     do
         echo PIHOLE_DNS_${i}=${ip}#53 >> /etc/pihole/setupVars.conf
         echo server=${ip}#53 >> /etc/dnsmasq.d/01-pihole.conf
-        i=$(($i+1))
     done < /tmp/ip
     
     # Syntax checking
